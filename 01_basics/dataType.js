67350a16a6f03c243587388389562631360e40d3
rachana2026 <65dchauhan@gmail.com>
2025-08-18 13:45:08 +0000

    datatypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,21 @@
+"use strict" // treate all JS code as never version
+
+
+
+let name = "rachana"
+let age = 19
+let  isLogginedIn = false
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null);  // object
