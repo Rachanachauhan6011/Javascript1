51178318202ae8df4b5b9b3bd981655a06a7f8e2
rachana2026 <65dchauhan@gmail.com>
2025-08-18 13:45:56 +0000

    datatypes

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "rach@gmail.com"
+var accountState = "jaipur"
+accountNumber = 25363;
+
+accountEmail = "r@gmail.com"
+accountState = "Benguluru"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountState , accountNumber]);
