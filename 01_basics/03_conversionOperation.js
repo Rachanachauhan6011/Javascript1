100d077ea33de224ddd8f389b618cf0d5a10445c
rachana2026 <65dchauhan@gmail.com>
2025-08-19 03:54:07 +0000

    datatype-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,22 @@
 let score = 33
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueNumber = String(score);
 
-console.log(typeof valueNumber);
+// console.log(typeof valueNumber);
+
+// console.log(2**2) //power
+
+
+// console.log("1" +2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+// console.log(+true);
+// console.log(+"")
+
+
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,34 @@
+
+
+
+
+const id = Symbol('123');
+
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+
+const bigNumber =  3446789090043356788n
+
+
+//Primitive datatype  =>   7 type =  number, string , boolean, bigint, null , undefined , Symbol
+
+// Reference(Non- primitive ) =>   array,  object, function
+
+
+const heros = ["shaktiman" ,"naagraj", "doga"]
+
+let myObj = {
+    nmae: "rachana",
+    age: 19,
+}
+
+
+const myfunction = function(){
+console.log("hello world");
+
+}
+
+
+console.log(typeof myfunction)
