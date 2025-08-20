a13b6430b5c3272b1b3854e6114d16528f1ab355
rachana2026 <65dchauhan@gmail.com>
2025-08-20 02:08:26 +0000

    numberandMath

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,32 @@
+const score = 400;
+// console.log(score)
+
+const balance  = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const anothernumber = 23.83652
+// console.log(anothernumber.toPrecision(3))
+
+// const number = 10000000;
+// console.log(number.toLocaleString('en-IN'))
+
+
+//++++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.6))
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10));
+
+
+
+
+
